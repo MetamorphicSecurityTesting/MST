a52c42b42486a38700b4525057d674070d440d55
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2022-02-14 12:42:51 -0500

    update

--- src/smrl/mr/crawljax/WebProcessor.java
+++ src/smrl/mr/crawljax/WebProcessor.java
@@ -955,8 +955,7 @@ public class WebProcessor {
 							String userParam = sysConfig.getSignupUserParam().trim();
 							if(userParam!= null &&
 									userParam.equals(idValue)){
-								//Fabrizio: 2022.02.08 commnting out the fllowing, not good here, randomization should be in the MR
-//								valueToSend += RandomStringUtils.random(5,true,false);
+								valueToSend += RandomStringUtils.random(5,true,false);
 							}
 							else{
 								//Check if this input is the confirm password in the signup action
